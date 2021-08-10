5845bbb5c78c1cf263c973a2902bc05e29b9078e
Jonathan Lavigne <jlavigne@techso.com>
2021-08-10 12:37:46 -0400

    init commit

---
+++ src/main/kotlin/ca/techso/jlavignelearn/JlavignelearnApplication.kt
@@ -0,0 +1,11 @@
+package ca.techso.jlavignelearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JlavignelearnApplication
+
+fun main(args: Array<String>) {
+	runApplication<JlavignelearnApplication>(*args)
+}
---
+++ src/test/kotlin/ca/techso/jlavignelearn/JlavignelearnApplicationTests.kt
@@ -0,0 +1,13 @@
+package ca.techso.jlavignelearn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JlavignelearnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
